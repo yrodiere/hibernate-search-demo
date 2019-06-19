c0e642a4fbede90f8aa3a705218cbf5cd4dd63e2
Guillaume Smet <guillaume.smet@gmail.com>
2019-06-19 14:10:56 +0200

    Initialize empty Quarkus project

---
+++ src/test/java/org/acme/hibernate/search/demo/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.acme.hibernate.search.demo;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
